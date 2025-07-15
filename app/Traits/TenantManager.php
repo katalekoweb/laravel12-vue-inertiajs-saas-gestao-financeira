18767b5f76edbb11846fc2faa957438a10679348
Kataleko <katalekoweb@gmail.com>
2025-07-15 19:33:29 +0100

    feat: multitenant manager

---
+++ app/Traits/TenantManager.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
+use App\Models\Tenant;
+use App\Models\User;
+use Ramsey\Uuid\Uuid;
+
+trait TenantManager
+{
+    public static function bootTenantManager()
+    {
+        static::creating(function (Model $model) {
+            $user = Auth::user();
+            if ($user) {
+                if (empty($model->user_id)) {
+                    $model->user_id = $user->id;
+                }
+                if (empty($model->tenant_id)) {
+                    $model->tenant_id = $user->tenant_id;
+                }
+            }
+            if (empty($model->uuid)) {
+                $model->uuid = (string) Uuid::uuid4();
+            }
+        });
+
+        static::addGlobalScope('tenant', function (Builder $builder) {
+            $user = Auth::user();
+            if ($user && $user->tenant_id) {
+                $builder->where('tenant_id', $user->tenant_id);
+            }
+        });
+    }
+
+    /**
+     * Get the tenant for the current record.
+     *
+     * @return Tenant|null
+     */
+    public function tenant(): ?Tenant
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+
+     /**
+     * Get the current user for the record.
+     *
+     * @return User|null
+     */
+    public function user(): ?User
+    {
+        return $this->belongsTo(User::class);
+    }
+}
